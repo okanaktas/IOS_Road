ccfa4a0324b4c4a38b2e94ff7a9a3201359655c9
Okan Aktaş <okanaktas02@gmail.com>
2025-03-10 23:49:54 +0300

    feature(lower and uppercase)

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+//Deneme
+
+var exampleNumber = 5
+print(exampleNumber)
+
+var userName = "Okan"
+var userSurname = "Aktas"
+
+print("isminiz ve soyisminiz: \(userName) \(userSurname)")
+
+userName.append("Mı?")
+userName.lowercased()
+userName.uppercased()
+----------------------------------------
