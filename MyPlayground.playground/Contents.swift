6108a1252b419dab35b8f272ff8f2f09bb79abbe
Okan Aktaş <okanaktas02@gmail.com>
2025-03-11 00:26:07 +0300

    feature(dizide son elemanı bulma)

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -34,3 +34,18 @@ print("Dizinin elemanları:", terminator: " ")
 for j in myArray {
     print(j, terminator: " ")
 }
+
+//------------------
+var exampleArray = ["merhaba",2,"Deneme",4] as [Any]
+//any olarak tanımlamanın dezavantajı fonksiyonları kullanamam mesela:
+//exampleArray[0].uppercased() çalışmaz ama dizi içerisinde sadece stringler olsa ver as yani casting yapmasaydık o zaman çalışacaktı
+
+var exampleArray2 = ["merhaba","dunya","Deneme","eatrh"]
+print(exampleArray2[0].uppercased())
+
+print("Dizinin son elamanı: \(exampleArray2.last!)")
+//ya da
+print("Dizinin son elamanı: ")
+let lastElement = exampleArray2[exampleArray2.count - 1]
+print(lastElement)
+print("Dizinin son elamanı: \(exampleArray2.count - 1)") //bu yanlış
