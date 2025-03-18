d22c9f9e2edc9fbba3bc8d04d09357c58ca59949
Okan Aktaş <okanaktas02@gmail.com>
2025-03-18 21:35:29 +0300

    feature(func)

--- FunctionsAndIlerisi.playground/Contents.swift
+++ FunctionsAndIlerisi.playground/Contents.swift
@@ -1,3 +1,28 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func myfunction(){
+    print("Hello World!")
+}
+
+print("Bakalım hangisi önce çalışacak")
+
+myfunction( )
+
+//Input & Output 6 Return
+
+func sumFunction(x: Int, y: Int) -> Int{
+    return x + y
+}
+
+print(sumFunction(x: 5, y: 6))
+
+
+func booleanFunc(a: Int, b : Int) -> Bool{
+    if a > b{
+        return true
+    }else{
+        return false
+    }
+}
+
+print(booleanFunc(a: 5, b: 6))
