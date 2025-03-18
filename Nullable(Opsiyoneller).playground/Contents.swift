a03ccc4436431ce63d14029ea683bf0cf45f1a7e
Okan Aktaş <okanaktas02@gmail.com>
2025-03-18 23:40:42 +0300

    feature(if let and ??(default value))

--- Nullable(Opsiyoneller).playground/Contents.swift
+++ Nullable(Opsiyoneller).playground/Contents.swift
@@ -5,3 +5,9 @@ var myValue = "asfadsg"
 var myNewValue = (Int(myValue) ?? 0) * 5
 
 print(myNewValue)
+
+if let myLastValue = Int(myValue){
+    print(myLastValue * 5)
+}else{
+    print("Wrong Value")
+}
