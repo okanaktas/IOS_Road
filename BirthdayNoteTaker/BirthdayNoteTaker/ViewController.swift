a72d3ca8302d525bee41610a78289b614c6cad71
Okan Aktaş <okanaktas02@gmail.com>
2025-03-25 13:01:35 +0300

    feature(User Defaults)

--- BirthdayNoteTaker/BirthdayNoteTaker/ViewController.swift
+++ BirthdayNoteTaker/BirthdayNoteTaker/ViewController.swift
@@ -16,12 +16,26 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        //Casting - as? String demek, değeri String'e çevirebiliyorsan çevir öyle devam et demek.
+        //force casting - as! String demek, ne olursa olsun String olarak kaydedeceksin bunu demek.
+        if let newName = storedName as? String{
+            nameLabel.text = newName
+        }
+        if let newBirthday = storedBirthday as? String{
+            birthdayLabel.text = newBirthday
+        }
+        
     }
 
     @IBAction func saveButton(_ sender: Any) {
         nameLabel.text = "Name: \(nameText.text ?? "")"
         birthdayLabel.text = "Birthday: \(birthdayText.text ?? "")"
+        
+        UserDefaults.standard.set(nameText.text,forKey: "name")
+        UserDefaults.standard.set(birthdayText.text,forKey: "birthday")
     }
     
 }
