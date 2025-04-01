02f93317577e4d89fe85b5bd7c71466ef71e4fa2
Okan Aktaş <okanaktas02@gmail.com>
2025-04-01 16:59:02 +0300

    UserDefaults

--- UserDefaults/UserDefaults/ViewController.swift
+++ UserDefaults/UserDefaults/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var textFieldBirthday: UITextField!
+    
+    @IBOutlet weak var labelInputed: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        labelInputed.text = UserDefaults.standard.string(forKey: "inputData") ?? "Name and Birthday:"
+        
     }
 
-
+    @IBAction func buttonSave(_ sender: Any) {
+        let inputValue = "Name and Birthday: \(textFieldName.text ?? "") \(textFieldBirthday.text ?? "")"
+        labelInputed.text = inputValue
+        
+        UserDefaults.standard.set(inputValue, forKey: "inputData")
+    }
+    
+    @IBAction func buttonDeleted(_ sender: Any) {
+        UserDefaults.standard.removeObject(forKey: "inputData")
+        labelInputed.text = "Name and Birthday: "
+    }
+    
 }
 
