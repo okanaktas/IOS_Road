0bcbbab275e3eca154fe1c1fd8dd71d50f56f3b6
Okan Aktaş <okanaktas02@gmail.com>
2025-04-08 17:41:46 +0300

    Segue
    performSegue

---
+++ Recap/Recap/SecondViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondViewController.swift
+//  Recap
+//
+//  Created by Okan Aktas on 8.04.2025.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var secondVCLabel: UILabel!
+    
+    @IBOutlet weak var inputedLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+
+}
--- Recap/Recap/ViewController.swift
+++ Recap/Recap/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var birthdayText: UITextField!
+    @IBOutlet weak var stored: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let inputedName = UserDefaults.standard.object(forKey: "name") as? String ?? ""
+        let inputedBirthday = UserDefaults.standard.object(forKey: "birthday") as? String ?? ""
+        stored.text = "Name: \(inputedName) Birthday: \(inputedBirthday)"
+        
     }
 
+    @IBAction func buttonSave(_ sender: Any) {
+        stored.text = "Name: \(nameText.text ?? "Name and Birthday: ") Birthday: \(birthdayText.text ?? "Name and Birthday: ")"
+        UserDefaults.standard.set(nameText.text, forKey: "name")
+        UserDefaults.standard.set(birthdayText.text, forKey: "birthday")
+        
+    }
+    @IBAction func deleteButton(_ sender: Any) {
+        UserDefaults.standard.removeObject(forKey: "name")
+        UserDefaults.standard.removeObject(forKey: "birthday")
+        stored.text = "Name and Birthday:"
+        
+    }
 
+    @IBAction func transferDatasButton(_ sender: Any) {
+        performSegue(withIdentifier: "goToSecondPage", sender: nil)
+    }
+    
+    
 }
 
