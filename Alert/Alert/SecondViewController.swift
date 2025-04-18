4c39ffbfce6adeb3f74d9bfef7cd73885140f70f
Okan Aktaş <okanaktas02@gmail.com>
2025-04-18 20:59:40 +0300

    Alert

---
+++ Alert/Alert/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  Alert
+//
+//  Created by Okan Aktas on 18.04.2025.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    var inputedValue = ""
+    
+    @IBOutlet weak var inputedLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        inputedLabel.text = inputedValue
+        
+    }
+
+}
--- Alert/Alert/ViewController.swift
+++ Alert/Alert/ViewController.swift
@@ -4,16 +4,46 @@
 //
 //  Created by Okan Aktas on 18.04.2025.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var valueTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func sendButton(_ sender: Any) {
+    
+        if valueTextField.text == "" {
+            //Gösterilecek ana alert UIAlertController
+            let alert = UIAlertController(title: "Alert Title", message: "Sen şimdi naneyi yemedin mi ?", preferredStyle: UIAlertController.Style.alert)
+            //Onaylama için oluşturduğum
+            let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default) { UIAlertAction in
+                print("Ok")
+            }
+            //oluşturduğumuz okButton'u alert içerisine gömmek için
+            alert.addAction(okButton)
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        performSegue(withIdentifier: "sendToSecondVC", sender: nil)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "sendToSecondVC" {
+            let destinationVC = segue.destination as! SecondViewController
+            destinationVC.inputedValue = valueTextField.text ?? ""
+        }
+    }
+    
 }
+
+
 
