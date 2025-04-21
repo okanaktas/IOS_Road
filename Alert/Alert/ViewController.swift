720b39a81fc891b9ae69535b2278287f137bea5a
Okan Aktaş <okanaktas02@gmail.com>
2025-04-21 17:15:41 +0300

    Alert

---
+++ Alert/Alert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alert
+//
+//  Created by Okan Aktas on 21.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
