6c34deddd01a80ba3e58efc3a946eaa4fb5ab37c
Okan Aktaş <okanaktas02@gmail.com>
2025-05-09 17:19:35 +0300

    feature(added views and images)

---
+++ Catch the Kenny/Catch the Kenny/GameViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GameViewController.swift
+//  Catch The Kenny
+//
+//  Created by Okan Aktas on 9.05.2025.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    var counterTime = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+
+}
--- Catch the Kenny/Catch the Kenny/ViewController.swift
+++ Catch the Kenny/Catch the Kenny/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lastScoreLabel: UILabel!
+    
+    @IBOutlet weak var enterCounterText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func startGameButton(_ sender: Any) {
+        performSegue(withIdentifier: "startGame", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "startGame" {
+            if let destination = segue.destination as? GameViewController {
+                destination.counterTime = Int(enterCounterText.text ?? "30") ?? 30
+            }
+        }
+    }
+    
 }
 
