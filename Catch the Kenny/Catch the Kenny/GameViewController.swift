d018251dee84f20a65a41a72965407ccc98bca3e
Okan Aktaş <okanaktas02@gmail.com>
2025-05-14 17:13:43 +0300

    update

--- Catch the Kenny/Catch the Kenny/GameViewController.swift
+++ Catch the Kenny/Catch the Kenny/GameViewController.swift
@@ -9,12 +9,29 @@ import UIKit
 
 class GameViewController: UIViewController {
     
-    var counterTime = 0
+    var counterTime: Int = 30
 
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var kenny0image: UIImageView!
+    @IBOutlet weak var kenny1image: UIImageView!
+    @IBOutlet weak var kenny2image: UIImageView!
+    @IBOutlet weak var kenny3image: UIImageView!
+    @IBOutlet weak var kenny4image: UIImageView!
+    @IBOutlet weak var kenny5image: UIImageView!
+    @IBOutlet weak var kenny6image: UIImageView!
+    @IBOutlet weak var kenny7image: UIImageView!
+    @IBOutlet weak var kenny8image: UIImageView!
+    @IBOutlet weak var kenny9image: UIImageView!
+    @IBOutlet weak var kenny10image: UIImageView!
+    @IBOutlet weak var kenny11image: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        counterLabel.text = "Counter: \(counterTime)"
 
     }
 
