c6de7e88e6e73de84ef770d8d3915292fa9c1e1a
Okan Aktaş <okanaktas02@gmail.com>
2025-05-16 20:08:52 +0300

    Create the Recap

---
+++ Recap/Recap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recap
+//
+//  Created by Okan Aktas on 16.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
