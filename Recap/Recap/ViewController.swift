a0bb5cdba4eefdbed5a27f074d47aee83737df16
Okan Aktaş <okanaktas02@gmail.com>
2025-05-21 00:43:00 +0300

    Recap

--- Recap/Recap/ViewController.swift
+++ Recap/Recap/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var counterLabel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    @IBAction func playGameButton(_ sender: Any) {
+        performSegue(withIdentifier: "playGame", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "playGame" {
+            let destinationViewController = segue.destination as! GameViewController
+            destinationViewController.counter = Int(counterLabel.text ?? "10") ?? 10
+        }
+    }
 
 }
 
