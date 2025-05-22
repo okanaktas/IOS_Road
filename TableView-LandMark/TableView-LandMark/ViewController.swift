ef6d1a062dbc87a8985d207a58f7fdca8868565a
Okan Aktaş <okanaktas02@gmail.com>
2025-05-23 01:32:23 +0300

    Created TableView

---
+++ TableView-LandMark/TableView-LandMark/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  TableView-LandMark
+//
+//  Created by Okan Aktas on 23.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+    }
+
+
+}
+
