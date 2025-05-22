64fb8198f0d8b7fd6aa4cd930a46b317fe03fb91
Okan Aktaş <okanaktas02@gmail.com>
2025-05-23 01:50:37 +0300

    TableView-LandMark

--- TableView-LandMark/TableView-LandMark/ViewController.swift
+++ TableView-LandMark/TableView-LandMark/ViewController.swift
@@ -8,22 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    
     @IBOutlet weak var tableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
     
+    //Kaç tane girdi olacağını soruyor bize, kaç tane göstereceksek onu yazıyoruz.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 10
     }
+    
+    //Burada ne göstereceğini soruyor. cell dediği hücre demek. TableView içerisinde ki görünümlerin her birine hücre diyoruz.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            
+        //TableView içerisinde cell adında bir hücre oluşturdum ama bu hücre içerisinde ne gösterilecek henüz söylemedim.
+        let cell = UITableViewCell()
+        /*eski kullanılmayan
+        cell.textLabel?.text = "test"
+         */
+        //içerik diye bir değişken oluşturuyorum ve içerik ayarlayıcısını içerisine atıyorum.
+        var content = cell.defaultContentConfiguration( )
+        content.text = "test"
+        content.secondaryText = "test2"
+        content.image = UIImage(systemName: "star")
+        //yapılan değişiklikleri eşitledim.
+        cell.contentConfiguration = content
+        return cell
     }
-
-
+    
+    
 }
 
