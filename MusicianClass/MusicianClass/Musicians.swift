a5822a7cbca2269523a390b3195dc249c16f8cef
Okan Aktaş <okanaktas02@gmail.com>
2025-05-23 22:34:15 +0300

    Created Musicians OOP

---
+++ MusicianClass/MusicianClass/Musicians.swift
@@ -0,0 +1,16 @@
+//
+//  Musicians.swift
+//  MusicianClass
+//
+//  Created by Okan Aktas on 23.05.2025.
+//
+
+import Foundation
+
+class Musicians{
+    
+    var name = ""
+    var age = 0
+    var instrument = ""
+    
+}
---
+++ MusicianClass/MusicianClass/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  MusicianClass
+//
+//  Created by Okan Aktas on 23.05.2025.
+//
+
+import Foundation
+
+print("Hello, World!")
+
